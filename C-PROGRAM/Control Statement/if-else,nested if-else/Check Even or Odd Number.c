138e9d177c179793f92527ce00447b90b94608b4
AMIT--GUPTA <ak4039496@gmail.com>
2026-07-18 11:12:22 +0530

    Check Even or Odd Number

---
+++ C-PROGRAM/Control Statement/if-else,nested if-else/Check Even or Odd Number.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    // Input from user
+    printf("Enter a number:- ");
+    scanf("%d", &num);
+
+    // If condition to check even number
+    if (num % 2 == 0) {
+        printf("%d is Even\n", num);
+    }
+    // Else condition for odd number
+    else {
+        printf("%d is Odd\n", num);
+    }
+
+    return 0;
+}
+
