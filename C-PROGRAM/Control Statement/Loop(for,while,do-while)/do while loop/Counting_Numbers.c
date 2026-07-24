dfca6a9bf293b26c971647e9bba6afb8a16e524e
AMIT--GUPTA <ak4039496@gmail.com>
2026-07-24 08:45:42 +0530

    Counting_Numbers

---
+++ C-PROGRAM/Control Statement/Loop(for,while,do-while)/do while loop/Counting_Numbers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int i; 
+
+    i = 1; // Initialization
+    do {
+        printf("Number:- %d\n", i);
+        i++; // Increment
+    } while(i <= 10); // Condition Check
+
+    return 0;
+}
