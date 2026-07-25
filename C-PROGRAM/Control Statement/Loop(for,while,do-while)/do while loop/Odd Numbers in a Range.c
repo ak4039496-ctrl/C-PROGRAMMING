684131b64fd5a00be3920d016a3cf4c6be350bba
AMIT--GUPTA <ak4039496@gmail.com>
2026-07-25 05:59:16 +0530

    Odd Numbers in a Range

---
+++ C-PROGRAM/Control Statement/Loop(for,while,do-while)/do while loop/Odd Numbers in a Range.c
@@ -0,0 +1,14 @@
+/*Author:- Amit Kumar
+data :- 20-07-2026*/
+#include <stdio.h>
+int main() {
+    int i;
+
+    i = 1;
+    do {
+        printf("%d ", i);
+        i += 2;
+    } while(i <= 20);
+
+    return 0;
+}
