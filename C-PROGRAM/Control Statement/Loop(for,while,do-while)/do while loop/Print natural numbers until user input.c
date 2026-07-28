e78ca30d334415debeb3bb8a3fd3d9082a171ff9
AMIT--GUPTA <ak4039496@gmail.com>
2026-07-28 06:43:03 +0530

    Print natural numbers until user input

---
+++ C-PROGRAM/Control Statement/Loop(for,while,do-while)/do while loop/Print natural numbers until user input.c
@@ -0,0 +1,15 @@
+/*
+Author: Amit Gupta
+Date: 19-07-2026
+Print natural numbers up to user input
+*/
+#include <stdio.h>
+int main() {
+    int n = 15, i = 1;
+    do {
+        printf("%d ", i);
+        i++;
+    } while (i <= n);
+    return 0;
+}
+
