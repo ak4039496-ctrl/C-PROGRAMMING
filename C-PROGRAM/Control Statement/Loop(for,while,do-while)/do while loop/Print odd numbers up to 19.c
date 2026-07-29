4873445943b95da927c4385cde17e57143923363
AMIT--GUPTA <ak4039496@gmail.com>
2026-07-29 07:56:33 +0530

    Print odd numbers up to 19

---
+++ C-PROGRAM/Control Statement/Loop(for,while,do-while)/do while loop/Print odd numbers up to 19.c
@@ -0,0 +1,15 @@
+/*
+Author: Amit Gupta
+Date: 18-07-2026
+Print odd numbers from 1 to 19
+*/
+#include <stdio.h>
+int main() {
+    int i = 1;
+    do {
+        printf("%d\n", i);
+        i += 2;
+    } while (i <= 19);
+    return 0;
+}
+
