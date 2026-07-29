1a63ca08754759763394278a62bb972658185eed
AMIT--GUPTA <ak4039496@gmail.com>
2026-07-29 07:56:07 +0530

    Print numbers in reverse (10 to 1)

---
+++ C-PROGRAM/Control Statement/Loop(for,while,do-while)/do while loop/Print numbers in reverse (10 to 1).c
@@ -0,0 +1,15 @@
+/*
+Author: Amit Gupta
+Date: 15-07-2026
+Print numbers from 10 down to 1 
+*/
+#include <stdio.h>
+int main() {
+    int i = 10;
+    do {
+        printf("%d\n", i);
+        i--;
+    } while (i >= 1);
+    return 0;
+}
+
