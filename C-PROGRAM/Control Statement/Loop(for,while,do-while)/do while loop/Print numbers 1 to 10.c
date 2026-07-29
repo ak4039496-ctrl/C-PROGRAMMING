ec4355445fe4627a11c5f705a3a23e8bcc5d2ea4
AMIT--GUPTA <ak4039496@gmail.com>
2026-07-29 07:55:10 +0530

    Print numbers 1 to 10

---
+++ C-PROGRAM/Control Statement/Loop(for,while,do-while)/do while loop/Print numbers 1 to 10.c
@@ -0,0 +1,15 @@
+/*
+Author: Amit Gupta
+Date: 13-07-2026
+Print numbers from 1 to 10
+*/
+#include <stdio.h>
+int main() {
+    int i = 1;
+    do {
+        printf("%d\n", i);
+        i++;
+    } while (i <= 10);
+    return 0;
+}
+
