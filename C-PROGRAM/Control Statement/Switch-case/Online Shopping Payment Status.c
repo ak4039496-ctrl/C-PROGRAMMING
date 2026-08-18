31591f3d18bac89b74597b4ffaf6afa46887fc73
AMIT--GUPTA <ak4039496@gmail.com>
2026-08-18 22:22:48 +0530

    Online Shopping Payment Status

---
+++ C-PROGRAM/Control Statement/Switch-case/Online Shopping Payment Status.c
@@ -0,0 +1,23 @@
+// Author: Amit Gupta
+// Date: 24 July 2026
+#include <stdio.h>
+
+int main() {
+    int payment_status = 1; // 1 = Paid, 2 = Pending, 3 = Failed
+
+    switch(payment_status) {
+        case 1:
+            printf("Payment completed\n");
+            break;
+        case 2:
+            printf("Payment pending\n");
+            break;
+        case 3:
+            printf("Payment failed\n");
+            break;
+        default:
+            printf("Invalid payment status\n");
+    }
+    return 0;
+}
+
